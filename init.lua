44b8c15d3585c7860c27a056a8da65a0a64243f4
曹阳兴 <caoyangxing@outlook.com>
2025-09-05 14:29:44 +0800

    fix:go

--- init.lua
+++ init.lua
@@ -1,2 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
+-- lua/config/dap-log.lua
+vim.fn.setenv("DAP_LOG_LEVEL", "DEBUG")
+vim.fn.setenv("DAP_LOG_FILE", vim.fn.stdpath("data") .. "/dap.log")
+
 require("config.lazy")
--- lua/plugins/go.lua
+++ lua/plugins/go.lua
@@ -20,36 +20,36 @@ return {
   {
     "mfussenegger/nvim-dap",
     config = function()
-      local dap = require("dap")
-
-      dap.set_log_level("DEBUG")
-      dap.adapters.delve = {
-        type = "server",
-        host = "127.0.0.1",
-        port = 38697,
-      }
-      dap.adapters.delve = function(callback, config)
-        callback({
-          type = "server",
-          port = "${port}",
-          executable = {
-            command = "/home/caoyx/go/bin/dlv",
-            args = { "dap", "-l", "127.0.0.1:${port}", "--log", "--log-output=dap", "--only-same-user=false" },
-            detached = true,
-          },
-        })
-      end
-
-      -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
-      dap.configurations.go = {
-        {
-          type = "delve",
-          name = "caoyxDebug",
-          request = "launch",
-          program = "${file}",
-          outputMode = "remote",
-        },
-      }
+      --      local dap = require("dap")
+      --
+      --      dap.set_log_level("DEBUG")
+      --      dap.adapters.delve = {
+      --        type = "server",
+      --        host = "127.0.0.1",
+      --        port = 38697,
+      --      }
+      --      dap.adapters.delve = function(callback, config)
+      --        callback({
+      --          type = "server",
+      --          port = "${port}",
+      --          executable = {
+      --            command = "/home/caoyx/go/bin/dlv",
+      --            args = { "dap", "-l", "127.0.0.1:${port}", "--log", "--log-output=dap", "--only-same-user=false" },
+      --            detached = true,
+      --          },
+      --        })
+      --      end
+      --
+      --      -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+      --      dap.configurations.go = {
+      --        {
+      --          type = "delve",
+      --          name = "caoyxDebug",
+      --          request = "launch",
+      --          program = "${file}",
+      --          outputMode = "remote",
+      --        },
+      --      }
     end,
   },
 }
