4204cc9dd1e37b12d54f9c3ebdd3c8f14f557818
曹阳兴 <caoyangxing@outlook.com>
2026-03-11 15:08:00 +0800

    fix: ansi

---
+++ lua/plugins/others.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "0xferrous/ansi.nvim",
+    config = function()
+      require("ansi").setup({
+        auto_enable = true, -- Auto-enable for configured filetypes
+        filetypes = { "log", "ansi" }, -- Filetypes to auto-enable
+        theme = "dracula",
+      })
+    end,
+  },
+}
